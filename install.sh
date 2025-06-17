3e992421715ff71f7e42b135327254e16aa5835f
goldentree1 <goldentreedev@gmail.com>
2025-06-17 16:24:00 +1200

    cleanup

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 glib-compile-schemas schemas
-zip gnome-command-menu-2.zip icon.svg metadata.json extension.js prefs.js schemas/org.gnome.shell.extensions.commandmenu2.gschema.xml schemas/gschemas.compiled README.md LICENSE
+zip gnome-command-menu-2.zip metadata.json extension.js prefs.js schemas/org.gnome.shell.extensions.commandmenu2.gschema.xml schemas/gschemas.compiled README.md LICENSE
 gnome-extensions install gnome-command-menu-2.zip --force
