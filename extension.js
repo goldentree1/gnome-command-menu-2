7889c83d16cf3dee6f8972d98ea7d52ccafd99ac
goldentree1 <goldentreedev@gmail.com>
2026-07-13 20:35:57 +1200

    dynamic-titles: extension works

--- extension.js
+++ extension.js
@@ -9,14 +9,116 @@ import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 
+Gio._promisify(Gio.Subprocess.prototype, 'communicate_utf8_async');
+
 const CommandMenuPopup = GObject.registerClass(
   class CommandMenuPopup extends PanelMenu.Button {
     _init(cmds, settings) {
       super._init(0.5);
       this.commands = cmds;
       this.commandMenuSettings = settings;
+      this._dynamicLabels = [];
+      this._signalIds = [];
+      this._timerIds = [];
+
+      this.connect('destroy', () => {
+        this._timerIds.forEach(id => GLib.source_remove(id));
+        this._timerIds = [];
+        this._signalIds.forEach(([obj, id]) => obj.disconnect(id));
+        this._signalIds = [];
+        this._dynamicLabels = [];
+      });
+
+      // render menu
       this.redrawMenu();
+
+      // asynchronously render dynamic titles
+      if (this.menu) {
+        const id = this.menu.connect('open-state-changed', (_menu, open) => {
+          if (open) this._refreshDynamics();
+        });
+        this._signalIds.push([this.menu, id]);
+      }
+      this._refreshDynamics(true);
     }
+
+    // dynamic title helpers...
+
+    _hasDynamicTitle(text) {
+      return typeof text === 'string' && text.includes('$(');
+    }
+
+    async _resolveDynamicTitle(text) {
+      if (!this._hasDynamicTitle(text)) return text;
+      try {
+        const proc = Gio.Subprocess.new(
+          ['bash', '-c', `printf '%s' "${text.replace(/"/g, '\\"')}"`],
+          Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_SILENCE,
+        );
+        const [stdout] = await proc.communicate_utf8_async(null, null);
+        return stdout || '';
+      } catch (e) {
+        logError(e, 'command-menu2: resolveDynamicTitle failed');
+        return text;
+      }
+    }
+
+    async _registerDynamicLabel(label, template, refreshInterval, parentMenu) {
+      const interval = Math.max(1, Number(refreshInterval)); // minimum 1s refresh
+      const entry = { label, template, interval, parentMenu, sourceId: 0 };
+      this._dynamicLabels.push(entry);
+
+      label.text = await this._resolveDynamicTitle(template);
+
+      if (!parentMenu) {
+        // no parent: always refresh
+        this._startTimer(entry);
+      } else {
+        // start refresh while parent menu/submenu is open, otherwise stop.
+        const id = parentMenu.connect('open-state-changed', (_menu, open) => {
+          if (open) this._startTimer(entry);
+          else this._stopTimer(entry);
+        });
+        this._signalIds.push([parentMenu, id]);
+        if (parentMenu.isOpen) this._startTimer(entry);
+      }
+    }
+
+    _startTimer(entry) {
+      if (entry.sourceId !== 0) return;
+      entry.sourceId = GLib.timeout_add_seconds(
+        GLib.PRIORITY_DEFAULT, entry.interval, () => {
+          this._resolveDynamicTitle(entry.template).then(text => {
+            entry.label.text = text;
+          }).catch(() => {
+            this._stopTimer(entry);
+            return GLib.SOURCE_REMOVE;
+          });
+          return GLib.SOURCE_CONTINUE;
+        },
+      );
+      this._timerIds.push(entry.sourceId);
+    }
+
+    _stopTimer(entry) {
+      if (entry.sourceId === 0) return;
+      GLib.source_remove(entry.sourceId);
+      this._timerIds = this._timerIds.filter(id => id !== entry.sourceId);
+      entry.sourceId = 0;
+    }
+
+    _refreshDynamics(forceAll = false) {
+      const entries = forceAll
+        ? this._dynamicLabels
+        : this._dynamicLabels.filter(e => !e.parentMenu || e.parentMenu.isOpen);
+      entries.forEach(entry => {
+        this._resolveDynamicTitle(entry.template).then(text => {
+          try { entry.label.text = text; } catch (e) {}
+        });
+      });
+    }
+
+    // main fns
 
     loadIcon(icon, style_class) {
       if (typeof icon !== 'string' || !icon.length) return null;
@@ -59,6 +161,9 @@ const CommandMenuPopup = GObject.registerClass(
           sectionLabel.actor.set_style('padding-top: 0px; padding-bottom: 0px; min-height: 0;');
           sectionLabel.actor.add_child(label);
 
+          if (cmd.dynamicTitle && this._hasDynamicTitle(cmd.title))
+            this._registerDynamicLabel(label, cmd.title, cmd.refreshInterval, menu);
+
           menu.addMenuItem(sectionLabel);
           return;
         }
@@ -70,6 +175,10 @@ const CommandMenuPopup = GObject.registerClass(
             const icon = this.loadIcon(cmd.icon, 'popup-menu-icon');
             if (icon) submenu.insert_child_at_index(icon, 1);
           }
+
+          if (cmd.dynamicTitle && this._hasDynamicTitle(cmd.title))
+            this._registerDynamicLabel(submenu.label, cmd.title, cmd.refreshInterval, menu);
+
           this.populateMenuItems(submenu.menu, cmd.submenu, level + 1);
           menu.addMenuItem(submenu);
           return;
@@ -87,6 +196,10 @@ const CommandMenuPopup = GObject.registerClass(
           y_align: Clutter.ActorAlign.CENTER
         });
         item.add_child(label);
+
+        if (cmd.dynamicTitle && this._hasDynamicTitle(cmd.title))
+          this._registerDynamicLabel(label, cmd.title, cmd.refreshInterval, menu);
+
         item.connect('activate', () => {
           GLib.spawn_command_line_async(cmd.command);
         });
@@ -95,10 +208,11 @@ const CommandMenuPopup = GObject.registerClass(
     }
 
     redrawMenu() {
-      // add popup menu title
+      // add menu title
       let menuTitle = this.commands.title ?? "";
       let box = new St.BoxLayout();
 
+      // add menu icon
       let icon = this.loadIcon(this.commands.icon, 'system-status-icon');
       if (!icon && menuTitle === "") { // fallback icon
         icon = new St.Icon({
@@ -119,6 +233,11 @@ const CommandMenuPopup = GObject.registerClass(
 
       box.add_child(text);
       this.add_child(box);
+
+      // dynamic titles: re-render every 'refreshInterval' seconds ONLY while parent menu is open
+      // of course, top-level items (i.e., gnome topbar menus) have no parent: always refresh.
+      if (this.commands.dynamicTitle && this._hasDynamicTitle(menuTitle))
+        this._registerDynamicLabel(text, menuTitle, this.commands.refreshInterval);
 
       // populate menu items
       if ((!Array.isArray(this.commands.menu) || this.commands.menu.length === 0)) {
